ce1b867c80cd22c42c2ac09d84477777edc1ef00
bryan <194650180+brainybryan69@users.noreply.github.com>
2025-08-17 20:50:45 +0800

    Added feature to mark tasks as done

--- src/main/java/LeBron.java
+++ src/main/java/LeBron.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class LeBron {
     public static void main(String[] args) {
         final String NAME = "LeBron";
-        String[] TASKS = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
         System.out.println("Hello! I'm " + NAME);
@@ -20,12 +20,37 @@ public class LeBron {
                 if (taskCount == 0) {
                     System.out.println("No tasks found.");
                 } else {
+                    System.out.println("Here are the tasks in your list:");
                     for (int i = 0; i < taskCount; i++) {
-                        System.out.println((i + 1) + ". " + TASKS[i]);
+                        System.out.println((i + 1) + "." + tasks[i].getStatusIcon() + " " + tasks[i].getDescription());
                     }
                 }
+            } else if (input.startsWith("mark ")) {
+                try {
+                    int taskIndex = Integer.parseInt(input.substring(5)) - 1;
+                    if (taskIndex >= 0 && taskIndex < taskCount) {
+                        Task task = tasks[taskIndex];
+                        task.markAsDone();
+                        System.out.println("Nice! I've marked this task as done:");
+                        System.out.println(task.getStatusIcon() + " " + task.getDescription());
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Invalid task number.");
+                }
+            } else if (input.startsWith("unmark ")) {
+                try {
+                    int taskIndex = Integer.parseInt(input.substring(7)) - 1;
+                    if (taskIndex >= 0 && taskIndex < taskCount) {
+                        Task task = tasks[taskIndex];
+                        task.markAsNotDone();
+                        System.out.println("OK, I've marked this task as not done yet:");
+                        System.out.println(task.getStatusIcon() + " " + tasks[taskIndex].getDescription());
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Invalid task number.");
+                }
             } else {
-                TASKS[taskCount] = input;
+                tasks[taskCount] = new Task(input);
                 taskCount++;
                 System.out.println("added: " + input);
             }
