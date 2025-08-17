97e89c4b62e621058ab437977d13edad7abb530b
bryan <194650180+brainybryan69@users.noreply.github.com>
2025-08-17 19:33:12 +0800

    Added feature that stores and lists tasks

--- src/main/java/LeBron.java
+++ src/main/java/LeBron.java
@@ -2,8 +2,11 @@ import java.util.Scanner;
 
 public class LeBron {
     public static void main(String[] args) {
-        String name = "LeBron";
-        System.out.println("Hello! I'm " + name);
+        final String NAME = "LeBron";
+        String[] TASKS = new String[100];
+        int taskCount = 0;
+
+        System.out.println("Hello! I'm " + NAME);
         System.out.println("What can I do for you?");
         
         Scanner scanner = new Scanner(System.in);
@@ -13,8 +16,19 @@ public class LeBron {
             input = scanner.nextLine();
             if (input.equals("bye")) {
                 break;
+            } else if (input.equals("list")) {
+                if (taskCount == 0) {
+                    System.out.println("No tasks found.");
+                } else {
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println((i + 1) + ". " + TASKS[i]);
+                    }
+                }
+            } else {
+                TASKS[taskCount] = input;
+                taskCount++;
+                System.out.println("added: " + input);
             }
-            System.out.println(input);
         }
         
         System.out.println("Bye. Hope to see you again soon!");
