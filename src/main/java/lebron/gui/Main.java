bbfceb1e5eec9f99a1470e02c0afa049c9a8c293
bryan <194650180+brainybryan69@users.noreply.github.com>
2025-09-15 18:21:26 +0800

    Add user guide

--- src/main/java/lebron/gui/Main.java
+++ src/main/java/lebron/gui/Main.java
@@ -23,6 +23,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("LeBron");
             fxmlLoader.<MainWindow>getController().setLeBron(lebron);  // inject the Duke instance
             stage.show();
         } catch (IOException e) {
